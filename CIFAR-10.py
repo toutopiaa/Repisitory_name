6c9b2c428a20250273767358d1ddf2f6e27977bc
toutopiaa <toutopia@qq.com>
2025-09-25 17:04:45 +0800

    Update CIFAR-10.py

--- CIFAR-10.py
+++ CIFAR-10.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
